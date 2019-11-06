0113d03daa0e955d290118321943546015ddcc49
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-11-06 23:02:10 +0100

    Add missing index for better search performance

--- 3166-country/001-3166_country_struture.sql
+++ 3166-country/001-3166_country_struture.sql
@@ -9,3 +9,18 @@ CREATE TABLE IF NOT EXISTS `country` (
   `a3` char(3) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Create name index
+--
+ALTER TABLE `country` ADD INDEX `idx_country_name` (`name`);
+
+--
+-- Create a2 index
+--
+ALTER TABLE `country` ADD UNIQUE `idx_unq_country_a2` (`a2`);
+
+--
+-- Create a3 index
+--
+ALTER TABLE `country` ADD UNIQUE `idx_unq_country_a3` (`a3`);
