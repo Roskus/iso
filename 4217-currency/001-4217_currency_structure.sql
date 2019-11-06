cce259f98aca0c77c2118be5cae97513d5ec15f2
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-11-06 23:49:03 +0100

    Rename sql for ordered execution. table singular name. Add index

---
+++ 4217-currency/001-4217_currency_structure.sql
@@ -0,0 +1,15 @@
+--
+-- Table structure for table `currency`
+--
+CREATE TABLE `currency` (
+  `code` char(3) COLLATE utf8_unicode_ci NOT NULL,
+  `money_name` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `symbol` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index for table `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`code`) USING BTREE,
+  ADD KEY `idx_money_name` (`money_name`);
