8dace7f0e43eb4911a838d63ca811fbc3fa92006
Gustavo Novaro <gnovaro@users.noreply.github.com>
2019-11-06 23:54:08 +0100

    Reduce money name to 60 chars
    Based in max money leght size is 56
    SELECT money_name, LENGTH(money_name) AS cant FROM `currency` ORDER BY cant DESC LIMIT 1

--- 4217-currency/001-4217_currency_structure.sql
+++ 4217-currency/001-4217_currency_structure.sql
@@ -3,7 +3,7 @@
 --
 CREATE TABLE `currency` (
   `code` char(3) COLLATE utf8_unicode_ci NOT NULL,
-  `money_name` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `money_name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `symbol` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
